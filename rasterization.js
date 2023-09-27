f7be4ff8184cb7115b331d3b1e539592b647119a
LiQingyang <liqingyang@xiaomi.com>
2023-09-27 19:44:06 +0800

    Filled Triangles

--- rasterization.js
+++ rasterization.js
@@ -44,8 +44,42 @@ function drawLine(P0, P1, color) {
   }
 }
 
+function drawWireFrameTriangle(P0, P1, P2) {
+  drawLine(P0, P1, [0, 0, 0]);
+  drawLine(P1, P2, [0, 0, 0]);
+  drawLine(P2, P0, [0, 0, 0]);
+}
+
+function drawFilledTriangle(oriP0, oriP1, oriP2, color) {
+  const [[x0, y0], [x1, y1], [x2, y2]] = [oriP0, oriP1, oriP2].sort(
+    (a, b) => a[1] - b[1]
+  );
+  const x01 = interpolate(y0, x0, y1, x1);
+  const x12 = interpolate(y1, x1, y2, x2);
+  const x02 = interpolate(y0, x0, y2, x2);
+
+  x01.splice(x01.length - 1, 1);
+  const x012 = x01.concat(x12);
+
+  const m = Math.floor(x012.length / 2);
+  let xLeft, xRight;
+  if (x02[m] < x012[m]) {
+    xLeft = x02;
+    xRight = x012;
+  } else {
+    xLeft = x012;
+    xRight = x02;
+  }
+
+  for (let y = y0; y <= y2; y++) {
+    for (let x = xLeft[y - y0]; x <= xRight[y - y0]; x++) {
+      canvas.putPixel(x, y, color);
+    }
+  }
+}
+
 function go() {
-  drawLine([-200, -100], [240, 120], [0, 0, 0]);
-  drawLine([-50, -200], [60, 240], [0, 0, 0]);
+  drawFilledTriangle([-200, -250], [200, 50], [20, 250], [0, 255, 0]);
+  drawWireFrameTriangle([-200, -250], [200, 50], [20, 250]);
 }
 go();
